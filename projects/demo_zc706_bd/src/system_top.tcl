bbef50f17f600c161e9d6575a6e223cac554b2a4
iamtonyxu <yalong.xu@foxmail.com>
2025-03-10 09:33:36 +0800

    opt on demo_zc706_bd tcl scripts

--- projects/demo_zc706_bd/src/system_top.tcl
+++ projects/demo_zc706_bd/src/system_top.tcl
@@ -1,59 +1,82 @@
-# Create a new project
+###########################################
+# System Top Level TCL Script
+# Function: Create block design with button debouncers
+###########################################
+
+# Basic configuration
 set bd_top "system_top"
-create_bd_design ${bd_top}
+set NUM_BUTTONS 4
+set IP_REPO_PATH "../../ip_gen"
+
+# Create new block design
+if {[catch {create_bd_design ${bd_top}} result]} {
+    puts "ERROR: Failed to create block design - $result"
+    return -code error
+}
 update_compile_order -fileset sources_1
 
-# Add sources to the project
-set_property  ip_repo_paths  ../../ip_gen [current_project]
-update_ip_catalog
+# Set IP repository path
+if {[catch {
+    set_property ip_repo_paths ${IP_REPO_PATH} [current_project]
+    update_ip_catalog
+} result]} {
+    puts "ERROR: Failed to set IP repository path - $result"
+    return -code error
+}
 
-# Add IP to the block design
+# Define procedure to create debouncer
+proc create_debouncer {index} {
+    if {[catch {
+        # Create debouncer IP instance
+        create_bd_cell -type ip -vlnv xilinx.com:user:PushButton_Debouncer:1.0 PushButton_Debouncer_${index}
+        
+        # Create and connect ports
+        create_bd_port -dir I switch_${index}
+        create_bd_port -dir O led_${index}
+        
+        # Connect clock and signals
+        connect_bd_net [get_bd_ports clk] [get_bd_pins PushButton_Debouncer_${index}/clk]
+        connect_bd_net [get_bd_ports switch_${index}] [get_bd_pins PushButton_Debouncer_${index}/PB]
+        connect_bd_net [get_bd_ports led_${index}] [get_bd_pins PushButton_Debouncer_${index}/PB_state]
+    } result]} {
+        puts "ERROR: Failed to create debouncer ${index} - $result"
+        return -code error
+    }
+}
+
+# Create clock port
+create_bd_port -dir I clk
+
+# Create debouncer instances
 startgroup
-create_bd_cell -type ip -vlnv xilinx.com:user:PushButton_Debouncer:1.0 PushButton_Debouncer_0
-create_bd_cell -type ip -vlnv xilinx.com:user:PushButton_Debouncer:1.0 PushButton_Debouncer_1
-create_bd_cell -type ip -vlnv xilinx.com:user:PushButton_Debouncer:1.0 PushButton_Debouncer_2
-create_bd_cell -type ip -vlnv xilinx.com:user:PushButton_Debouncer:1.0 PushButton_Debouncer_3
+for {set i 0} {$i < $NUM_BUTTONS} {incr i} {
+    create_debouncer $i
+}
 endgroup
 
-# Add ports to the block design
-create_bd_port -dir I clk
-connect_bd_net [get_bd_ports clk] [get_bd_pins PushButton_Debouncer_0/clk]
-connect_bd_net [get_bd_ports clk] [get_bd_pins PushButton_Debouncer_1/clk]
-connect_bd_net [get_bd_ports clk] [get_bd_pins PushButton_Debouncer_2/clk]
-connect_bd_net [get_bd_ports clk] [get_bd_pins PushButton_Debouncer_3/clk]
+# Generate and save design
+if {[catch {
+    regenerate_bd_layout
+    save_bd_design
+} result]} {
+    puts "ERROR: Failed to generate or save design - $result"
+    return -code error
+}
 
-create_bd_port -dir I switch_0
-connect_bd_net [get_bd_ports switch_0] [get_bd_pins PushButton_Debouncer_0/PB]
-create_bd_port -dir I switch_1
-connect_bd_net [get_bd_ports switch_1] [get_bd_pins PushButton_Debouncer_1/PB]
-create_bd_port -dir I switch_2
-connect_bd_net [get_bd_ports switch_2] [get_bd_pins PushButton_Debouncer_2/PB]
-create_bd_port -dir I switch_3
-connect_bd_net [get_bd_ports switch_3] [get_bd_pins PushButton_Debouncer_3/PB]
-
-create_bd_port -dir O led_0
-connect_bd_net [get_bd_ports led_0] [get_bd_pins PushButton_Debouncer_0/PB_state]
-create_bd_port -dir O led_1
-connect_bd_net [get_bd_ports led_1] [get_bd_pins PushButton_Debouncer_1/PB_state]
-create_bd_port -dir O led_2
-connect_bd_net [get_bd_ports led_2] [get_bd_pins PushButton_Debouncer_2/PB_state]
-create_bd_port -dir O led_3
-connect_bd_net [get_bd_ports led_3] [get_bd_pins PushButton_Debouncer_3/PB_state]
-
-# Generate the block design layout
-regenerate_bd_layout
-
-# Save the block design
-save_bd_design
-
-
+# Create HDL wrapper
 set project_name [get_property NAME [current_project]]
+set bd_file "./build/${project_name}.srcs/sources_1/bd/${bd_top}/${bd_top}.bd"
+set wrapper_file "./build/${project_name}.gen/sources_1/bd/${bd_top}/hdl/${bd_top}_wrapper.v"
 
-make_wrapper -files [get_files ./build/${project_name}.srcs/sources_1/bd/${bd_top}/${bd_top}.bd] -top
+if {[catch {
+    make_wrapper -files [get_files ${bd_file}] -top
+    add_files -norecurse ${wrapper_file}
+    update_compile_order -fileset sources_1
+    set_property top ${bd_top}_wrapper [current_fileset]
+} result]} {
+    puts "ERROR: Failed to create or add HDL wrapper - $result"
+    return -code error
+}
 
-add_files -norecurse ./build/${project_name}.gen/sources_1/bd/${bd_top}/hdl/${bd_top}_wrapper.v
-
-update_compile_order -fileset sources_1
-
-set_property top ${bd_top}_wrapper [current_fileset]
+puts "INFO: Block design creation completed successfully"
 
